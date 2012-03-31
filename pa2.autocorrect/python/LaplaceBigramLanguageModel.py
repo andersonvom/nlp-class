62d3ebe2f6bfd905917828e2be78b3c4e4600522
Anderson Mesquita <andersonvom@gmail.com>
2012-03-31 19:13:29 -0400

    Implemented Bigram language model using Laplace Add_1 smoothing

--- pa2.autocorrect/python/LaplaceBigramLanguageModel.py
+++ pa2.autocorrect/python/LaplaceBigramLanguageModel.py
@@ -1,20 +1,38 @@
+import math
+from collections import Counter
+from collections import defaultdict
+
 class LaplaceBigramLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.count = defaultdict(Counter)
     self.train(corpus)
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
-    pass
+    for sentence in corpus.corpus:
+      prev_word = None
+      for datum in sentence.data:
+        word = datum.word
+        self.count[prev_word][word] += 1
+        prev_word = word
+      self.count[prev_word][None] += 1 # make sure even the last word gets counted
+    self.vocabulary_size = len(self.count) - 1 # 'None' is not part of the vocabulary
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    # TODO your code here
-    return 0.0
+    score = 0.0
+    prev_word = None
+    for token in sentence:
+      if prev_word != None:
+        two_words_count = self.count[prev_word][token] + 1.0
+        prev_word_count = sum(self.count[prev_word].values()) + self.vocabulary_size
+        score += math.log(two_words_count)
+        score -= math.log(prev_word_count)
+      prev_word = token
+    return score
