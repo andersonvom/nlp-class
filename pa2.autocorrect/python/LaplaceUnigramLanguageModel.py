f05e1d66a1b5ffa5ca31ee868e3be74de3634066
Anderson Mesquita <andersonvom@gmail.com>
2012-03-31 16:58:06 -0400

    Implemented Unigram Language Model (without add_1 smoothing)

--- pa2.autocorrect/python/LaplaceUnigramLanguageModel.py
+++ pa2.autocorrect/python/LaplaceUnigramLanguageModel.py
@@ -1,20 +1,30 @@
+from collections import *
+
 class LaplaceUnigramLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.count = Counter()
+    self.vocabulary_size = 0
+    self.num_words = 0
     self.train(corpus)
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
-    pass
+    for sentence in corpus.corpus:
+      for datum in sentence.data:
+        word = datum.word
+        self.count[word] += 1
+    self.vocabulary_size = len(self.count.items())
+    self.num_words = sum(self.count.values())
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    # TODO your code here
-    return 0.0
+    score = 0.0
+    for token in sentence:
+      score += (self.count[token] * 1.0) / ( self.num_words )
+    return score
