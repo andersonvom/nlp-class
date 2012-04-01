2d9a17b5d9a8a7896fb7d33488f9fda51c9bb97a
Anderson Mesquita <andersonvom@gmail.com>
2012-04-01 18:29:23 -0400

    Restarted Stupid Backoff from scratch, this time using only bigrams and unigrams. Better accuracy.

--- pa2.autocorrect/python/StupidBackoffLanguageModel.py
+++ pa2.autocorrect/python/StupidBackoffLanguageModel.py
@@ -6,12 +6,11 @@ class StupidBackoffLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    self.n_gram = 2
-    self.backoff_modifier = 0.4
-    self.count = []
-    for i in range(self.n_gram):
-      self.count.append(Counter())
-    self.total_words = 0
+    self.unigram_count = Counter()
+    self.bigram_count = defaultdict(Counter)
+    self.vocabulary_size = 0
+    self.num_words = 0
+    self.backoff_multiplier = 0.4
     self.train(corpus)
 
   def train(self, corpus):
@@ -19,59 +18,32 @@ class StupidBackoffLanguageModel:
         Compute any counts or other corpus statistics in this function.
     """  
     for sentence in corpus.corpus:
-      prev_words = [None] * self.n_gram
+      prev_word = None
       for datum in sentence.data:
         word = datum.word
-        
-        prev_words.append(word)
-        prev_words = prev_words[1:]
-        if prev_words[0] == None:
-          continue
-        
-        # count all groups of words in the sentence
-        for i in range(self.n_gram):
-          num_words = i + 1
-          word_tuple = tuple(prev_words[0:num_words])
-          self.count[i][word_tuple] += 1
+        self.unigram_count[word] += 1
+        if prev_word != None:
+          self.bigram_count[prev_word][word] += 1
+        prev_word = word
       
-      # count the remaining words in the sentence
-      prev_words.reverse()
-      prev_words.pop()
-      for word in prev_words:
-        self.count[0][(word)] += 1
-      
-    self.vocabulary_size = len(self.count[0])
-    self.total_words = sum(self.count[0].values())
+    self.vocabulary_size = len(self.unigram_count)
+    self.num_words = sum(self.unigram_count.values())
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
     score = 0.0
-    prev_words = [None] * self.n_gram
+    prev_word = None
     for token in sentence:
-      prev_words.append(token)
-      prev_words = prev_words[1:]
-      if prev_words[0] == None:
-        continue
-      
-      curr_n_gram = self.n_gram - 1
-      while (curr_n_gram > 0):
-        curr_idx = self.n_gram - 1 - curr_n_gram
-        all_words = tuple( prev_words[curr_idx:] )
-        if self.count[curr_n_gram][all_words] == 0:
-          curr_n_gram -= 1
-          continue
-        all_words_count = self.count[curr_n_gram][all_words]
-        prefix_words_count = self.count[curr_n_gram-1][all_words[0:-1]]
-        score += math.log(all_words_count)
-        score -= math.log(prefix_words_count)
-        break
-
-      if curr_n_gram == 0:
-        score += math.log( self.backoff_modifier )
-        score += math.log( self.count[0][(token)] + 1.0 )
-        score -= math.log( self.total_words + self.vocabulary_size )
-        
+      two_words_count = self.bigram_count[prev_word][token]
+      prev_word_count = self.unigram_count[prev_word]
+      if (two_words_count > 0):
+        score += math.log(two_words_count)
+        score -= math.log(prev_word_count)
+      else:
+        score += math.log(self.backoff_multiplier)
+        score += math.log(self.unigram_count[token] + 1.0)
+        score -= math.log(self.num_words + self.vocabulary_size)
+      prev_word = token
     return score
-
